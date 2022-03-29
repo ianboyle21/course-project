04f410854c0aca4e70c71cd59cd486ab69137a0c
Ian Boyle <ian.boyle@alight.com>
2022-03-29 15:34:08 -0400

    passes info from recipe-item to rcipe details

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+import {Recipe} from '../../recipe.model'
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() public recipeList;
+  @Input() public recipe: Recipe;
+  @Output() public recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
+
+  onSelect(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+    console.log("From Recipe item ", recipe)
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,8 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test.', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
+    new Recipe('A test recipe', 'This is a test.', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574'), new Recipe('A test recipe', 'This is a test.', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
   ];
+
+  @Output() loadedRecipe = new EventEmitter<Recipe>() ;
+  loadRecipe(recipe: Recipe){
+    this.loadedRecipe.emit(recipe)
+    console.log("From Recipe List: ", this.loadedRecipe)
+  }
   constructor() { }
 
   ngOnInit(): void {
